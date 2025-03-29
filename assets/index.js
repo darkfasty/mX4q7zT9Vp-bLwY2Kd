18b1e086600a7ac356b90ac88465d2c1159f7d5c
Your Name <your.email@example.com>
2025-03-29 14:43:28 +0100

    Updated website files

--- assets/index.js
+++ assets/index.js
@@ -57,7 +57,7 @@ imageInput.addEventListener('change', (event) => {
     fetch('https://api.imgur.com/3/image' ,{
         method: 'POST',
         headers: {
-            'Authorization': 'Client-ID 4ecc257cbb25ccc'
+            'Authorization': 'Client-ID a7a20a45c2eb3ff'
         },
         body: data
     })
